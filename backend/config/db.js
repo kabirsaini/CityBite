bc253271e8602d4f2f29208ec1268ff4e60d3795
kabirsaini <kabirrsaini7@gmail.com>
2025-09-02 14:40:01 +0530

    backend changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
 
-const connectDB = async()=>{
-    try{
+const connectDB = async () => {
+    try {
 
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, {
+
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+
+        });
         console.log("MongoDB connected successfully");
     }
-    catch(error){
+    catch (error) {
         console.error("Error connecting to MongoDB:", error.message);
         process.exit(1); // Exit the process with failure
     }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client'; //
 import App from './App.jsx';
 import './index.css';
 
