15db72772d20393c9d8f6c0211b9d4a9ae0f00b9
kabirsaini <kabirrsaini7@gmail.com>
2025-09-09 21:15:30 +0530

    .

--- frontend/src/Components/Auth/Signup.jsx
+++ frontend/src/Components/Auth/Signup.jsx
@@ -1,4 +1,5 @@
-import '@/Components/User/Style/Signup.css'; // new CSS file
+import "../User/Style/Login.css";
+
 import { GoogleLogin } from '@react-oauth/google';
 import { useForm } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom';
