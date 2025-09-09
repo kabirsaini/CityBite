8274cc7c6d36aa4fab9d230407b63659f5db051b
kabirsaini <kabirrsaini7@gmail.com>
2025-09-09 19:16:55 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -22,8 +22,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads'));
 
 app.use(cors({
-    origin: 'http://localhost:5173', // React app's origin
-    credentials: true
+    origin: ["https://food-website-backend-20z8.onrender.com", "http://localhost:5173"],
+    credentials: true,
 }));
 
 
